6095ef072d2d3a4531558482b39fd6fedfc38410
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-08-03 15:24:31 +0200

    created card forWeather

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Text} from './../Text/Text'
 import style from './WeatherCard.module.css'
 
 export const WeatherCard = () => {
   return (
     <div className={style.weathercard} >
-
+<div className={style.location} ><Text variant='h1'>locat..</Text></div>
     </div>
   )
 }
