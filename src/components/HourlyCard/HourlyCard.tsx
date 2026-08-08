73d7bf56e433883841f244222ab614fe501f0e1b
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-08 23:34:24 +0200

    Updated Hourlycard,added weatherIcon frmAPI

--- src/components/HourlyCard/HourlyCard.tsx
+++ src/components/HourlyCard/HourlyCard.tsx
@@ -1,62 +1,44 @@
 import React from 'react'
-import { SiRainmeter } from "react-icons/si"
+import WeatherIcon from '../WeatherIcon/WeatherIcon'
 import styles from './HourlyCard.module.css'
-import { format } from 'date-fns'
-import clearDay from '../../assets/icons/clear-day.png'
-import clearNight from '../../assets/icons/clear-night.png'
-import rainy from '../../assets/icons/rainy.png'
-import thunderstorm from '../../assets/icons/thunderstorm.png'
-import lightning from '../../assets/icons/lightning.png'
-import wind from '../../assets/icons/wind.png'
-import cloudy from '../../assets/icons/cloudy.png'
-import partlycloudyday from '../../assets/icons/partly-cloudy-day.png'
 
-const iconMap: Record<string, string> = {
-  'clear-day': clearDay,
-  'clear-night': clearNight,
-  'cloudy': cloudy,
-  'rainy': rainy,
-  'thunderstorm': thunderstorm,
-  'lightning': lightning,
-  'wind': wind,
-  'partly-cloudy-day': partlycloudyday,
+interface Props {
+  data: any[]
+  temperature: 'C' | 'F'
 }
+const HourlyCard: React.FC<Props> = ({ data, temperature }) => {
+  const convert = (temp: number) =>
+    temperature === 'F'
+      ? (temp * 9) /5+32
+      : temp
 
-interface HourlyData {
-  datetime: string
-  temp: number
-  conditions: string
-  icon: string
-  precipprob: number
-}
-
-interface HourlyCardProps {
-  data: HourlyData[]
-}
-
-export const HourlyCard: React.FC<HourlyCardProps> = ({ data }) => {
   return (
-   
-    <div className={styles.cardd}>
-     <span className={styles.hourText} >Hourly Forecast</span>
-     <div className={styles.hourContainer}>
-      {data.map((hour, index) => (
-        <div className={styles.card} key={index}>
-          <div className={styles.hour}>
-            {format(new Date(`1970-01-01T${hour.datetime}`), 'h a')}
-          </div>
-          <div>
-          <img
-            src={iconMap[hour.icon]} 
-            alt={hour.conditions} className={styles.icon}
-          /></div>
-          <div className={styles['hour-temp']}>{Math.round(hour.temp)}°</div>
-          <div className={styles['hour-rain']}><SiRainmeter /> {hour.precipprob}%</div>
-        </div>
-      ))}
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Hourly Forecast</h2>
+      <div className={styles.hourContainer}>
+        {data.map((hour, index) => {
+          const number = Number(hour.datetime.split(':')[0])
+          const hour12 = number % 12 || 12
+          const ampm = number < 12?'AM':'PM'
+          const displayTime = `${hour12} ${ampm}`
+          return (
+            <div
+              className={styles.card}
+              key={index}>
+              <div className={styles.time}>
+                {displayTime}</div>
+              <WeatherIcon icon={hour.icon} />
+              <div className={styles['hour-temp']}>
+                {Math.round(convert(hour.temp))}°{temperature}
+              </div>
+              <div>
+                {hour.precipprob}%
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
 }
-
 export default HourlyCard
