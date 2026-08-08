6df150f069838621995e1413389b2cea6cfd80fc
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-08 23:12:10 +0200

    updated weathercard,imported weatherIcon , removed assetsIcons

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,74 +1,44 @@
 import React from 'react'
+import WeatherIcon from '../WeatherIcon/WeatherIcon'
 import { format } from 'date-fns'
 import styles from './WeatherCard.module.css'
-import clearDay from '../../assets/icons/clear-day.png'
-import clearNight from '../../assets/icons/clear-night.png'
-import cloudy from '../../assets/icons/cloudy.png'
-import rainy from '../../assets/icons/rainy.png'
-import thunderstorm from '../../assets/icons/thunderstorm.png'
-import lightning from '../../assets/icons/lightning.png'
-import wind from '../../assets/icons/wind.png'
-import partlycloudyday from '../../assets/icons/partly-cloudy-day.png'
 
-const iconMap: Record<string, string> = {
-  'clear-day': clearDay,
-  'clear-night': clearNight,
-  'cloudy': cloudy,
-  'rainy': rainy,
-  'thunderstorm': thunderstorm,
-  'lightning': lightning,
-  'wind': wind,
-  'partly-cloudy-day': partlycloudyday,
+interface Props {
+  data: any
+  current: any
+  temperature: 'C' | 'F'
 }
-interface WeatherCardProps {
-  data: {
-    temp: number
-    tempmax: number
-    tempmin: number
-    conditions: string
-    icon: string
-    feelslike?: number
-    
-  }
-  
-}
-
-export const WeatherCard: React.FC<WeatherCardProps> = ({ data}) => {
-  const {
-    temp,
-    tempmax,
-    tempmin,
-    conditions,
-    icon,
-    feelslike,
-    
-  } = data
-
+const WeatherCard: React.FC<Props> = ({
+  data,
+  current,
+  temperature
+}) => {
+  const convert = (temp: number) =>
+    temperature === 'F'
+      ? (temp * 9) / 5+32
+      : temp
   return (
     <div className={styles.currentWeather}>
       <div className={styles.left}>
-        <div>
-          
-          <p>{format(new Date(), 'EEEE, MMMM d')}</p>
-          <h1 className="temp">{Math.round(temp)}°C</h1>
-          <p>
-            High: {Math.round(tempmax)}° / Low: {Math.round(tempmin)}°
-          </p>
-          {feelslike && <p>Feels like {Math.round(feelslike)}°</p>}
-          
-        </div>
-        </div>
-        <div className={styles.right}>
-          <img
-            src={iconMap[icon]} 
-            alt={conditions} className={styles.icon}
-          />
-          <h2 className="condition-text">{conditions}</h2>
-        
+        <p>{format(new Date(), 'EEEE, MMMM d')}</p>
+        <h1>
+          {Math.round(convert(current.temp))}
+          °{temperature}
+        </h1>
+        <p>High: {Math.round(convert(data.tempmax))}°
+          {'/ '}
+          Low: {Math.round(convert(data.tempmin))}°
+        </p>
+        <p>Feels like:{' '}
+          {Math.round(convert(current.feelslike))}°
+        </p>
       </div>
-      
+
+      <div className={styles.right}>
+        <WeatherIcon icon={current.icon} />
+        <h2>{current.conditions}</h2>
+      </div>
     </div>
   )
 }
-
 export default WeatherCard
