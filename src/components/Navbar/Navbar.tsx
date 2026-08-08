9251a98cc3d20261993ca0e5e79ca8ece39d50c1
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-08 22:08:11 +0200

    UPDATED Menu icon on Navbar to work forSettings

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,37 +1,34 @@
 import React from 'react'
-import { Text } from '../Text/Text';
-import { RxInfoCircled } from "react-icons/rx";
-import { CgMenuBoxed } from "react-icons/cg";
-import { BsSearch } from "react-icons/bs";
-import style from './Navbar.module.css'
-import SearchBar from '../SearchBar/SearchBar';
+import { useNavigate } from 'react-router-dom'
+import { RxInfoCircled } from 'react-icons/rx'
+import { CgMenuBoxed } from 'react-icons/cg'
+import SearchBar from '../SearchBar/SearchBar'
+import styles from './Navbar.module.css'
 
-
-interface NavbarProps {
-  onSearch: (query: string) => void;
+interface Props {
   location: string
-  onSettings: () => void
+  onSearch: (city: string) => void
 }
-
-export const Navbar = ({ onSearch, location, onSettings }: NavbarProps) => {
+export const Navbar: React.FC<Props> = ({
+  location,
+  onSearch
+  
+}) => {
+  const navigate = useNavigate()
   return (
     <nav>
-        <div className={style.infoIcon}>
-        <RxInfoCircled/>
-        </div>
-        
-        <div className={style.location} ><h2>{location}</h2></div>
-        <div className={style.icons} >
-        <div className={style.SearchIcon}><SearchBar onSearch={onSearch}/>
-        </div><BsSearch/>
-        <div
-  className={style.icon}
-  onClick={onSettings}
->
-  <CgMenuBoxed />
-</div>
-       </div>
-        
+      <RxInfoCircled className={styles.infoIcon} />
+      <div className={styles.location}>
+        <h2>{location}</h2>
+      </div>
+
+      <div className={styles.icons}>
+        <SearchBar onSearch={onSearch} />
+        <CgMenuBoxed
+          className={styles.icon}
+          onClick={() => navigate('/settings')}/>
+      </div>
     </nav>
   )
 }
+export default Navbar
