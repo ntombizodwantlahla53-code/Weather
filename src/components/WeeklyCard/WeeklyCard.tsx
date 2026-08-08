468771b47929753a17b3ea33d90c0a9c8ac0e443
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-09 00:32:13 +0200

    updated weeklyForecast

--- src/components/WeeklyCard/WeeklyCard.tsx
+++ src/components/WeeklyCard/WeeklyCard.tsx
@@ -1,89 +1,67 @@
 import React from 'react'
-import styles from './WeeklyCard.module.css'
 import { format } from 'date-fns'
-import clearDay from '../../assets/icons/clear-day.png'
-import clearNight from '../../assets/icons/clear-night.png'
-import cloudy from '../../assets/icons/cloudy.png'
-import rainy from '../../assets/icons/rainy.png'
-import thunderstorm from '../../assets/icons/thunderstorm.png'
-import lightning from '../../assets/icons/lightning.png'
-import wind from '../../assets/icons/wind.png'
-import partlycloudyday from '../../assets/icons/partly-cloudy-day.png'
+import WeatherIcon from '../WeatherIcon/WeatherIcon'
+import styles from './WeeklyCard.module.css'
+import { FaWind } from "react-icons/fa6";
+import { MdOutlineWaterDrop } from "react-icons/md";
 
-const iconMap: Record<string, string> = {
-  'clear-day': clearDay,
-  'clear-night': clearNight,
-  'cloudy': cloudy,
-  'rainy': rainy,
-  'thunderstorm': thunderstorm,
-  'lightning': lightning,
-  'wind': wind,
-  'partly-cloudy-day': partlycloudyday,
+interface Props {
+  data: any[]
+  temperature: 'C'|'F'
+  wind?: number
+  humidity?: number
 }
-
-interface DayData {
-  datetime: string
-  tempmax: number
-  tempmin: number
-  conditions: string
-  icon: string
-  precipprob: number
-}
-
-interface WeeklyCardProps {
-  data: DayData[]
-  currentWindspeed?: number 
-  currentHumidity?: number  
-}
-
-export const WeeklyCard: React.FC<WeeklyCardProps> = ({ 
-  data, 
-  currentWindspeed, 
-  currentHumidity 
+const WeeklyCard: React.FC<Props> = ({
+  data,
+  temperature,
+  wind,
+  humidity
 }) => {
+  const convert = (temp: number) =>
+    temperature === 'F'
+      ? (temp * 9) /5+32
+      : temp
   return (
-    <div className={styles.Container}>
-      <span className={styles.hourText} >Weekly Forecast</span>
-      <div className={styles.weeklyContainer}>
-      {data.map((day, index) => (
-        <div className={styles.day} key={index}>
-          <div className={styles.left}>
-            <div className={styles['day-label']}>
-              {format(new Date(day.datetime), 'EEE')}
+    <div className={styles.container}>
+      <div className={styles.weeklyCard}>
+        <h3 className={styles.WHeading}>Weekly Forecast</h3>
+        {data.map((day, index) => (
+          <div className={styles.day} key={index}>
+            <div className={styles.dayName}>
+              {format(new Date(day.datetime),'EEE, d')}
             </div>
-            
-            <div className={styles['day-rain']}>
+            <WeatherIcon icon={day.icon}/>
+            <div className={styles.condition}>
+              {day.conditions}</div>
+            <div className={styles.rain}>
               {day.precipprob}%
             </div>
-            <div className={styles['day-condition']}>
-              <img src={iconMap[day.icon]} alt={day.conditions} className={styles.icon} />
-              <span className={styles['day-text']}>{day.conditions}</span>
-            </div>
-            <div className={styles['day-temp']}>
-              {Math.round(day.tempmin)}° / {Math.round(day.tempmax)}°
+            <div className={styles.temperature}>
+              {Math.round(convert(day.tempmin))}°
+              {'/'}
+              {Math.round(convert(day.tempmax))}°
             </div>
           </div>
-          
-          <div className={styles.right}>
-            {/* Wind box container */}
-            {index === 0 && currentWindspeed !== undefined && (
-              <div className={styles.Box}>
-                <p>Wind: {currentWindspeed} km/h</p>
-              </div>
-            )}
-            
-            {/* Humidity box container */}
-            {index === 0 && currentHumidity !== undefined && (
-              <div className={styles.HBox}>
-                <p>Humidity: {currentHumidity}%</p>
-              </div>
-            )}
+        ))}
+
+      </div>
+      <div className={styles.rightCard}>
+        <div className={styles.wind}>
+          <span><FaWind/></span>
+          <div >
+            <p>Wind</p>
+            <h3>{wind??''} km/h</h3>
           </div>
         </div>
-      ))}
-    </div>
+        <div className={styles.hum}>
+          <span><MdOutlineWaterDrop/></span>
+          <div>
+            <p>Humidity</p>
+            <h3>{humidity??''}%</h3>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default WeeklyCard
